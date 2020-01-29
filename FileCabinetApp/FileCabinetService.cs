94d0c814d3ba5fdb726bd7902df11030d31d2783
Nikita Demidenko <nikita.demidenko24@gmail.com>
2020-01-29 20:12:11 +0300

    Add implementation for edit command.

--- FileCabinetApp/FileCabinetService.cs
+++ FileCabinetApp/FileCabinetService.cs
@@ -69,5 +69,67 @@ namespace FileCabinetApp
         public FileCabinetRecord[] GetRecords() => this.list.ToArray();
 
         public int GetStat() => this.list.Count;
+
+        public void EditRecord(int id, string firstName, string lastName, DateTime dateOfBirth, char sex, short numberOfReviews, decimal salary)
+        {
+            if (id < 1 || id > this.list.Count)
+            {
+                throw new ArgumentException($"There is no #{id} record.");
+            }
+
+            if (firstName == null)
+            {
+                throw new ArgumentNullException(nameof(firstName));
+            }
+
+            if (lastName == null)
+            {
+                throw new ArgumentNullException(nameof(lastName));
+            }
+
+            if (firstName.Length < 2 || firstName.Length > 60)
+            {
+                throw new ArgumentException("First name's length is out of range.");
+            }
+
+            if (lastName.Length < 2 || lastName.Length > 60)
+            {
+                throw new ArgumentException("Last name's length is out of range.");
+            }
+
+            if (dateOfBirth <= new DateTime(1950, 01, 01) || dateOfBirth > DateTime.Now)
+            {
+                throw new ArgumentException("Invalid date.");
+            }
+
+            if (sex != 'M' && sex != 'F')
+            {
+                throw new ArgumentException("There're only two genders.");
+            }
+
+            if (salary < 0)
+            {
+                throw new ArgumentException("Salary cannot be less than zero.");
+            }
+
+            if (numberOfReviews < 0)
+            {
+                throw new ArgumentException("Number of reviews cannot be less than zero.");
+            }
+
+            foreach (var record in this.list)
+            {
+                if (record.Id == id)
+                {
+                    record.FirstName = firstName;
+                    record.LastName = lastName;
+                    record.DateOfBirth = dateOfBirth;
+                    record.Sex = sex;
+                    record.NumberOfReviews = numberOfReviews;
+                    record.Salary = salary;
+                    return;
+                }
+            }
+        }
     }
 }
